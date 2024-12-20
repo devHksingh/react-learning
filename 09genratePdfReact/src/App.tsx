f8ee2d78a64e69d96d98e55ef96174dc80f7678f
devHksingh <hksingh.dev@gmail.com>
2024-12-20 19:14:41 +0530

    project setup

---
+++ 09genratePdfReact/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='w-screen min-h-screen p-4 bg-slate-950 text-slate-200'>
+        <h1 className='capitalize'>generate PDF in React using React pdf</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
