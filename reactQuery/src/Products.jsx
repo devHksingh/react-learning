9566ebe9af7b1a27024168c45e107710db8cec86
devHksingh <hksingh.dev@gmail.com>
2025-01-23 21:19:57 +0530

    daTA FETCHING FORM REACT QUERY

--- reactQuery/src/Products.jsx
+++ reactQuery/src/Products.jsx
@@ -1,32 +1,43 @@
-import { useEffect,useState } from "react";
+// import { useEffect,useState } from "react";
 import axios from "axios";
+import {Link} from 'react-router-dom'
+import { useQuery } from "@tanstack/react-query";
 
 const Products = () => {
-    const [products,setProducts]= useState([])
-    const [error,setError]= useState(false)
-    const [loading,setLoading]= useState(false)
-    
-    useEffect(()=>{
-        const fetchProduct =async ()=>{
-            try {
-              setError(false)
-              setLoading(true)
-              const res = await axios.get('https://dummyjson.com/products')
-            setProducts(res.data.
-                products
-                )
-            console.log(res.data);
-            } catch (error) {
-              setError(true)
-            }finally{
-              setLoading(false)
-            }
-            
-        }
-        fetchProduct()
+  const fetchProduct =async ()=>{
+    const res = await axios.get('https://dummyjson.com/products')
+    const data = await res.data.products
+    return data
+}
+  const {isLoading,error,data:products} = useQuery({ queryKey:["products"],queryFn:fetchProduct})
+  
 
-    },[])
-    if(loading){
+  
+    // const [products,setProducts]= useState([])
+    // const [error,setError]= useState(false)
+    // const [loading,setLoading]= useState(false)
+    
+    // useEffect(()=>{
+    //     const fetchProduct =async ()=>{
+    //         try {
+    //           setError(false)
+    //           setLoading(true)
+    //           const res = await axios.get('https://dummyjson.com/products')
+    //         setProducts(res.data.
+    //             products
+    //             )
+    //         console.log(res.data);
+    //         } catch (error) {
+    //           setError(true)
+    //         }finally{
+    //           setLoading(false)
+    //         }
+            
+    //     }
+    //     fetchProduct()
+
+    // },[])
+    if(isLoading){
       return (<><h1 className="text-2xl text-red-600 animate-pulse">Loading....</h1></>)
     }
     if(error){
@@ -35,7 +46,7 @@ const Products = () => {
     return (
         <>
         {products?(<div className="bg-white">
-      <div className="max-w-2xl px-4 py-16 mx-auto sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="max-w-2xl px-4 py-16 mx-auto sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 ">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
 
         <div className="grid grid-cols-1 mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
@@ -50,10 +61,10 @@ const Products = () => {
               <div className="flex justify-between mt-4">
                 <div>
                   <h3 className="text-sm text-gray-700">
-                    <a href="">
+                    <Link to={`/products/${product.id}`}>
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.title}
-                    </a>
+                    </Link>
                   </h3>
                   <p className="mt-1 text-sm text-gray-500">{product.category}</p>
                 </div>
--- reactQuery/src/main.jsx
+++ reactQuery/src/main.jsx
@@ -1,27 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Products from './Products.jsx';
-import Product from './Product.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Products from "./Products.jsx";
+import Product from "./Product.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const querClient = new QueryClient();
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-    },
-    {
-        path: '/products',
-        element: <Products />,
-    },
-    {
-        path: '/products/:productId',
-        element: <Product />,
-    },
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/products/:productId",
+    element: <Product />,
+  },
 ]);
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <QueryClientProvider client={querClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
