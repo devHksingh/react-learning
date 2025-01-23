4d6ba417c9ff356decf6ce2f5ae128633a396894
devHksingh <hksingh.dev@gmail.com>
2025-01-23 23:45:16 +0530

    add mutation

--- reactQuery/src/Product.jsx
+++ reactQuery/src/Product.jsx
@@ -1,11 +1,16 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 import { useParams } from "react-router-dom";
 
 const Product = () => {
     const {productId}= useParams()
-    console.log(productId, typeof(productId))
+    // console.log(productId, typeof(productId))
+    const mutation = useMutation({
+        mutationFn:(newTitle)=>{
+            return axios.put(`https://dummyjson.com/products/${(productId)}`,newTitle)
+        }
+    })
     const fetchSingleProduct = async()=>{
         const res = await axios.get(`https://dummyjson.com/products/${(productId)}`)
         console.log(res.data)
@@ -44,6 +49,27 @@ const Product = () => {
                 <p>${data.price}</p>  
                 <h3>{data.tags.map((tag,i)=>(<p key={i}>{tag}</p>))}</h3>
             </div>
+            <div>
+      {mutation.isPending ? (
+        'Adding todo...'
+      ) : (
+        <>
+          {mutation.isError ? (
+            <div>An error occurred: {mutation.error.message}</div>
+          ) : null}
+
+          {mutation.isSuccess ? <div><h1 className="text-xl font-bold capitalize border border-b">title added!</h1></div> : null}
+
+          <button className="p-1 bg-orange-400 rounded"
+            onClick={() => {
+              mutation.mutate({  title: 'Dummy title' })
+            }}
+          >
+            Update title
+          </button>
+        </>
+      )}
+    </div>
         </>
     );
 };
