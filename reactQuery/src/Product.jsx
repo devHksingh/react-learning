7f63d2b9758378ec8f2b90ce0cc7e12eda547786
devHksingh <hksingh.dev@gmail.com>
2025-01-23 23:21:03 +0530

    fetch single product

--- reactQuery/src/Product.jsx
+++ reactQuery/src/Product.jsx
@@ -1,5 +1,51 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+import { useParams } from "react-router-dom";
+
 const Product = () => {
-    return <div>Product</div>;
+    const {productId}= useParams()
+    console.log(productId, typeof(productId))
+    const fetchSingleProduct = async()=>{
+        const res = await axios.get(`https://dummyjson.com/products/${(productId)}`)
+        console.log(res.data)
+        return res.data
+    }
+    const {isLoading,error,data} = useQuery({
+        queryKey:["singleProduct",`${productId}`],
+        queryFn:fetchSingleProduct,
+        refetchInterval:22*1000,
+        refetchIntervalInBackground: true,
+    })
+    if (isLoading) {
+        return (
+          <>
+            <h1 className="text-2xl text-red-600 animate-pulse">Loading....</h1>
+          </>
+        );
+      }
+      if (error) {
+        return (
+          <>
+            <h1 className="text-2xl text-red-600 animate-pulse">
+              Error occured on fetch....{error.message}
+            </h1>
+          </>
+        );
+      }
+    return (
+        <>
+            <div>
+                <img src={`${data.images[0]}`} alt={data.title}/>
+                <h1>{data.title}</h1>
+                <p>{data.brand}</p>
+                <p>{data.category}</p>
+                <p>{data.description}</p>
+                <p>${data.price}</p>  
+                <h3>{data.tags.map((tag,i)=>(<p key={i}>{tag}</p>))}</h3>
+            </div>
+        </>
+    );
 };
 
 export default Product;
--- reactQuery/src/main.jsx
+++ reactQuery/src/main.jsx
@@ -10,7 +10,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 10*1000,
+      staleTime: 20*1000,
     },
     
   },
