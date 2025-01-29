9d0392086eb6c4740b8475536088976a8c6bc899
devHksingh <hksingh.dev@gmail.com>
2025-01-30 00:11:46 +0530

    minor ui fix

--- testAppWrite/src/components/NavBar.tsx
+++ testAppWrite/src/components/NavBar.tsx
@@ -10,29 +10,29 @@ const NavBar = () => {
     const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches
     if(isDark){
       setTheme("dark")
-      setIsOn(true)
+      setIsOn(false)
       localStorage.setItem("theme","dark")
       document.body.classList.add("dark")
     }else{
       setTheme("light")
-      setIsOn(false)
+      setIsOn(true)
       localStorage.setItem("theme","light")
       document.body.classList.add("light")
     }
   },[])
   useEffect(() => {
     document.body.classList.remove("light", "dark");
-    document.body.classList.add(isOn ? "dark" : "light");
-    localStorage.setItem("theme", isOn ? "dark" : "light");
-    setTheme(isOn ? "dark" : "light");
+    document.body.classList.add(isOn ? "light" : "dark");
+    localStorage.setItem("theme", isOn ? "light" : "dark");
+    setTheme(isOn ? "light" : "dark");
   }, [isOn]);
   return (
-    <nav className="sticky bottom-0 flex items-center justify-around w-full p-4 bg-stone-600 bg-opacity-40" >
+    <nav className="sticky bottom-0 flex items-center justify-around w-full p-4 bg-stone-600 bg-surface-tonal-a10" >
       <ul className="flex justify-center w-3/4 gap-12">
-        <li><NavLink to="/home" className={({isActive})=>`text-primary-a10 font-medium text-lg hover:text-primary-a50 ${isActive?` border-b-2 border-lime-400 pb-0.5 `:``}`}>Home</NavLink></li>
-        <li><NavLink to="/form" className={({isActive})=>`text-primary-a40 font-medium text-lg hover:text-primary-a50 ${isActive?`border-b-2 border-lime-400 pb-0.5`:``}`}>Form</NavLink></li>
-        <li><NavLink to="/show" className={({isActive})=>`text-primary-a40 font-medium text-lg hover:text-primary-a50 ${isActive?`border-b-2 border-lime-400 pb-0.5`:``}`}>Show</NavLink> </li>
-        <li><NavLink to="/login" className={({isActive})=>`text-primary-a40 font-medium text-lg hover:text-primary-a50 ${isActive?`border-b-2 border-lime-400 pb-0.5`:``}`}>Login</NavLink></li>
+        <li><NavLink to="/home" className={({isActive})=>`text-primary-a10 font-medium text-lg hover:text-primary-a50 ${isActive?` border-b-2 border-orange-400 pb-0.5 `:``}`}>Home</NavLink></li>
+        <li><NavLink to="/form" className={({isActive})=>`text-primary-a40 font-medium text-lg hover:text-primary-a50 ${isActive?`border-b-2 border-orange-400 pb-0.5`:``}`}>Form</NavLink></li>
+        <li><NavLink to="/show" className={({isActive})=>`text-primary-a40 font-medium text-lg hover:text-primary-a50 ${isActive?`border-b-2 border-orange-400 pb-0.5`:``}`}>Show</NavLink> </li>
+        <li><NavLink to="/login" className={({isActive})=>`text-primary-a40 font-medium text-lg hover:text-primary-a50 ${isActive?`border-b-2 border-orange-400 pb-0.5`:``}`}>Login</NavLink></li>
         
       </ul>
       
@@ -40,7 +40,7 @@ const NavBar = () => {
         {/* container */}
         <div
         className={`w-16 h-7 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer transition-all ${
-          isOn ? "bg-yellow-500" : "bg-gray-600"
+          isOn ? "bg-yellow-800" : "bg-gray-600"
         }`}
         onClick={() => setIsOn(!isOn)}
       >
@@ -50,7 +50,7 @@ const NavBar = () => {
           
         </div>
         {/* Label */}
-      <span className="ml-3 text-gray-400 transition-all">{isOn ? "Light " : "Dark "}Mode
+      <span className={`ml-3  capitalize transition-all text-sm`}>{theme} Mode
       </span>
       </div>
     
