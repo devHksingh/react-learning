408a4739d20a252ac216bf8250ed6b62286e26a6
devHksingh <hksingh.dev@gmail.com>
2025-01-30 22:28:50 +0530

    add route

--- testAppWrite/src/router.tsx
+++ testAppWrite/src/router.tsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import HomeLayout from "./Layout/HomeLayout";
-
-
+import FormPage from "./components/FormPage";
+import ShowPage from "./components/ShowPage";
+import LoginPage from "./components/LoginPage";
 
 const router = createBrowserRouter([
     {
@@ -12,7 +13,10 @@ const router = createBrowserRouter([
             {
                 path:"",
                 element:<Home/>
-            }
+            },
+            { path: "form", element: <FormPage /> },
+            { path: "show", element: <ShowPage /> },
+            { path: "login", element: <LoginPage /> },
         ]
     }
 ])
