699cefeb664ffb3013b0fae819c26be80fbaac07
devHksingh <hksingh.dev@gmail.com>
2025-01-30 21:36:21 +0530

    add react-query

--- testAppWrite/src/main.tsx
+++ testAppWrite/src/main.tsx
@@ -1,13 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 20*1000,
+    },
+    
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router}/>
-    <App />
+
+    </QueryClientProvider>
+    
   </StrictMode>,
 )
