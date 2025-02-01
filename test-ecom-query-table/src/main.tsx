a9fc50a330d6b18b202fe18cbd4b1f19d9191016
devHksingh <hksingh.dev@gmail.com>
2025-02-01 22:36:40 +0530

    setup store

--- test-ecom-query-table/src/main.tsx
+++ test-ecom-query-table/src/main.tsx
@@ -5,7 +5,8 @@ import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.tsx'
-
+import { Provider } from 'react-redux'
+import { store } from './app/store.ts'
 
 const queryClient = new QueryClient({
   defaultOptions:{
@@ -17,11 +18,13 @@ const queryClient = new QueryClient({
   }
 })
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router}/>
-    
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}/>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>,
 )
