53a0c42418076baf785566085e4a2d4bae42646f
devHksingh <hksingh.dev@gmail.com>
2025-02-01 21:49:46 +0530

    setup react query

--- test-ecom-query-table/src/main.tsx
+++ test-ecom-query-table/src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider } from 'react-router-dom'
+import router from './router.tsx'
+
+
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries: {
+      staleTime: 10*(60*1000), // 10 mins
+      gcTime:15*(60*1000), // 15 mins cacheTime
+      refetchIntervalInBackground:true
+    },
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router}/>
+    
+    </QueryClientProvider>
   </StrictMode>,
 )
