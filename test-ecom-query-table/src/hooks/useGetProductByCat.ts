aab6373bd575b2823042fe7ef37bc6d4b7949c59
devHksingh <hksingh.dev@gmail.com>
2025-02-02 21:51:44 +0530

    add hook for get product by category

---
+++ test-ecom-query-table/src/hooks/useGetProductByCat.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query"
+import { productByCategory } from "../http/api/api"
+
+const useGetProductByCat = (category:string) => {
+  const {data,isLoading}= useQuery({
+    queryKey:["category",`${category}`],
+    queryFn:()=>productByCategory(category)
+  })
+  return {data,isLoading}
+}
+
+export default useGetProductByCat
