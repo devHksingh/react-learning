7a844a35f9cc74bc0e136887ac97d82f158e5e0b
devHksingh <hksingh.dev@gmail.com>
2025-02-03 14:13:11 +0530

    add reducer

--- test-ecom-query-table/src/app/store.ts
+++ test-ecom-query-table/src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from '../features/product/cartSlice'
+import wishlistReducer from '../features/product/wishListSlice'
 export const store = configureStore({
-    reducer:{
-        cart:cartReducer
+    reducer: {
+        cart: cartReducer,
+        wishList: wishlistReducer
     },
 })
 
 export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch  = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch
