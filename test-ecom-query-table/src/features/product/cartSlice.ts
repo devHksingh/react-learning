da6b10f2079e84c2cbe4d5035d3ddb649e60a323
devHksingh <hksingh.dev@gmail.com>
2025-02-04 18:27:40 +0530

    add reducer for removeProductQuantity

--- test-ecom-query-table/src/features/product/cartSlice.ts
+++ test-ecom-query-table/src/features/product/cartSlice.ts
@@ -8,7 +8,7 @@ export interface CartItem {
 const initialState: CartItem[] = []
 
 export const cartSlice = createSlice({
-    name: 'cartItem ',
+    name: 'cart',
     initialState,
     reducers: {
         removeCartProduct: (state, action: PayloadAction<{ productId: number }>) => {
@@ -22,6 +22,17 @@ export const cartSlice = createSlice({
                 state.push({ productId: action.payload.productId, quantity: 1 })
             }
         },
+        removeProductQuantity:(state,action:PayloadAction<{ productId: number }>)=>{
+            const existingProduct = state.find((item)=>item.productId === action.payload.productId)
+            if(existingProduct){
+                const quantity = existingProduct.quantity
+                if(quantity===1){
+                    return state.filter((item)=>item.productId !== action.payload.productId)
+                }else if(quantity >1){
+                    existingProduct.quantity -=1
+                }
+            }
+        },
         clearCart: () => {
             return []
         }
@@ -29,6 +40,6 @@ export const cartSlice = createSlice({
 
 })
 
-export const { addCartProduct, clearCart, removeCartProduct } = cartSlice.actions
+export const { addCartProduct, clearCart, removeCartProduct,removeProductQuantity } = cartSlice.actions
 
 export default cartSlice.reducer
